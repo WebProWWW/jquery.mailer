469d9c50bd29239fcc501f16f2cd24d20ab3b7d0
Timur <v.timur8484@gmail.com>
2017-07-11 11:43:33 +0300

    first commit

---
+++ dist/jquery.mailer.js
@@ -0,0 +1,133 @@
+(function() {
+  var $, Mailer,
+    bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
+
+  $ = jQuery;
+
+  Mailer = (function() {
+    Mailer.prototype.settings = {
+      action: '/',
+      method: 'POST',
+      dataType: 'json',
+      emailRegex: /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i,
+      sendingStr: 'Sending...',
+      process: false
+    };
+
+    Mailer.prototype.sendingCurrentHtml = '';
+
+    function Mailer($form, options) {
+      this.onSubmit = bind(this.onSubmit, this);
+      $.extend(this.settings, options);
+      $form.bind('submit', this.onSubmit);
+    }
+
+    Mailer.prototype.onSubmit = function(e) {
+      var $form;
+      e.preventDefault();
+      $form = $(e.target);
+      if ((this.validate($form.find('[validate]'))) && !this.process) {
+        this.send($form);
+      }
+      return false;
+    };
+
+    Mailer.prototype.send = function($form) {
+      this.process = true;
+      this.progress($form);
+      $.ajax({
+        method: this.settings.method,
+        url: this.settings.action,
+        data: $form.serialize(),
+        dataType: this.settings.dataType
+      }).done((function(_this) {
+        return function(data) {
+          return _this.success($form, data);
+        };
+      })(this)).fail((function(_this) {
+        return function() {
+          return _this.error($form);
+        };
+      })(this)).always((function(_this) {
+        return function() {
+          _this.process = false;
+          return _this.always($form);
+        };
+      })(this));
+      return true;
+    };
+
+    Mailer.prototype.success = function($form, data) {
+      console.log('success($form, data)');
+      return console.log(data);
+    };
+
+    Mailer.prototype.error = function($form) {
+      return console.log('error($form)');
+    };
+
+    Mailer.prototype.always = function($form) {
+      var $sending;
+      $sending = $form.find('.js-mailer-progress');
+      $sending.html(this.sendingCurrentHtml);
+      return true;
+    };
+
+    Mailer.prototype.progress = function($form) {
+      var $sending;
+      $sending = $form.find('.js-mailer-progress');
+      this.sendingCurrentHtml = $sending.html();
+      $sending.html(this.settings.sendingStr);
+      return true;
+    };
+
+    Mailer.prototype.inputError = function($input) {
+      var $parent;
+      $parent = $input.parent();
+      $parent.addClass('has-error');
+      $input.one('focusin', function(e) {
+        return $(this).parent().removeClass('has-error');
+      });
+      return true;
+    };
+
+    Mailer.prototype.validate = function($inputs) {
+      var result;
+      result = true;
+      $inputs.each((function(_this) {
+        return function(i, input) {
+          var $input, inputRes, inputVal, validate;
+          $input = $(input);
+          validate = $input.attr('validate');
+          inputVal = $input.val();
+          inputRes = (function() {
+            switch (validate) {
+              case 'text':
+                return inputVal.length > 2;
+              case 'email':
+                return this.settings.emailRegex.test(inputVal);
+              default:
+                return true;
+            }
+          }).call(_this);
+          if (!inputRes) {
+            result = inputRes;
+            _this.inputError($input);
+          }
+          return true;
+        };
+      })(this));
+      return result;
+    };
+
+    return Mailer;
+
+  })();
+
+  $.fn.extend({
+    mailer: function(options) {
+      return new Mailer(this, options);
+    }
+  });
+
+}).call(this);
---
+++ src/jquery.mailer.coffee
@@ -0,0 +1,79 @@
+$ = jQuery
+
+class Mailer
+  settings:
+    action: '/'
+    method: 'POST'
+    dataType: 'json'
+    emailRegex: /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i
+    sendingStr: 'Sending...'
+    process: off
+  sendingCurrentHtml: ''
+
+  constructor: ($form, options) ->
+    $.extend @settings, options
+    $form.bind 'submit', @onSubmit
+
+  onSubmit: (e) =>
+    e.preventDefault()
+    $form = $ e.target
+    @send $form if (@validate $form.find '[validate]') and not @process
+    off
+
+  send: ($form) ->
+    @process = on
+    @progress $form
+    $.ajax
+      method: @settings.method
+      url: @settings.action
+      data: $form.serialize()
+      dataType: @settings.dataType
+    .done (data) => @success $form, data
+    .fail () => @error $form
+    .always () =>
+      @process = off
+      @always $form
+    on
+
+  success: ($form, data) ->
+    console.log 'success($form, data)'
+    console.log data
+
+  error: ($form) ->
+    console.log 'error($form)'
+
+  always: ($form) ->
+    $sending = $form.find '.js-mailer-progress'
+    $sending.html @sendingCurrentHtml
+    on
+
+  progress: ($form) ->
+    $sending = $form.find '.js-mailer-progress'
+    @sendingCurrentHtml = $sending.html()
+    $sending.html @settings.sendingStr
+    on
+
+  inputError: ($input) ->
+    $parent = $input.parent()
+    $parent.addClass 'has-error'
+    $input.one 'focusin', (e) ->
+      $(@).parent().removeClass 'has-error'
+    on
+
+  validate: ($inputs) ->
+    result = on
+    $inputs.each (i, input) =>
+      $input = $ input
+      validate = $input.attr 'validate'
+      inputVal = $input.val()
+      inputRes = switch validate
+        when 'text' then inputVal.length > 2
+        when 'email' then @settings.emailRegex.test inputVal
+        else on
+      unless inputRes
+        result = inputRes
+        @inputError $input
+      on
+    return result
+
+$.fn.extend mailer: (options) -> new Mailer @, options
